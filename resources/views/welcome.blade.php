646893bd79a0beef1f52bce14bc956b008da2b3d
Minakov Nikita <MinakovNV@users.noreply.github.com>
2020-04-03 10:50:04 +0600

    Dolpin smog commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Venik
+                    Venik(DolpinDilya smog)
                 </div>
 
                 <div class="links">
