c7d04885a2d4f67fd3bdacc0becc8a8d618e2aa6
AbSanzhar <mak.millan@mail.ru>
2020-04-07 02:36:45 +0600

    new_front

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,215 +1,64 @@
 <!DOCTYPE html>
-<html >
 <head>
-    <!-- Site made with Mobirise Website Builder v4.8.8, https://mobirisethemes.com -->
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="generator" content="Mobirise v4.8.8, mobirisethemes.com">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-    <link rel="shortcut icon" href="assets/images/logo2.png" type="image/x-icon">
-    <meta name="description" content="">
-    <title>SAND Social</title>
-    <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
-    <link rel="stylesheet" href="assets/tether/tether.min.css">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="assets/dropdown/css/style.css">
-    <link rel="stylesheet" href="assets/theme/css/style.css">
-    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title_lable')</title>
+
+    <nav class="navbar navbar-expand-sm bg-danger navbar-dark justify-content-end">
+        <a class="navbar-brand" href="/home">Venik Social</a>
+        <button class="btn ml-auto"></button>
+        <button class="navbar-toggler" type="button">
+        </button>
+        <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-right">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sport</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Science</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Culture</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Response</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Finance</a>
+                </li>
+
+                @if (Route::has('login'))
+
+                        @auth
+                            <a class="nav-link" href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a class="nav-link" href="{{ route('login') }}">Login</a>
+                            <a class="nav-link" href="{{ route('register') }}">Register</a>
+                        @endauth
+
+                @endif
 
 
-
-</head>
-<body>
-<section class="menu cid-rbrY7ExFmu" once="menu" id="menu1-m">
-
-
-
-    <nav class="navbar navbar-dropdown beta-menu align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
-
-        <div class="menu-bottom">
-
-            <div class="menu-logo">
-                <div class="navbar-brand">
-
-                <span class="navbar-caption-wrap"><a href="#" class="brand-link mbr-white display-4">
-                        Sangar/Alisher/Dilnaz/Nick</a></span>
-                </div>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <div class="hamburger">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </button>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav nav-dropdown js-float-line" data-app-modern-menu="true">
-                    <li class="nav-item">
-                        <a class="nav-link link mbr-white display-4" href="#">
-
-                            About
-                        </a>
-                    </li></ul>
-                <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-warning display-4" href="#">
-
-                        Log In</a></div>
-            </div>
+            </ul>
         </div>
     </nav>
-</section>
-
-<section class="header4 cid-r7bY6IxYOZ mbr-parallax-background" id="header4-2">
 
 
-
-    <div class="mbr-overlay" style="opacity: 0.7;">
     </div>
-
-    <div class="container align-center">
-        <div class="row justify-content-center">
-            <div class="media-container-column mbr-white col-md-10">
-                <h1 class="mbr-section-title pb-3 mbr-fonts-style display-1"><strong>SAND.Venik
-                    </strong><div><strong>Social</strong></div></h1>
-
-                <h3 class="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-5">First anti-quarantine social network.
-                    Connect and spend time together!</h3>
-
-
-                <div class="mbr-section-btn pt-3"><a class="btn btn-md btn-bgr btn-warning display-4" href="https://mobirisethemes.com">Sign In</a></div>
-            </div>
-        </div>
     </div>
+    </nav>
 
-</section>
+</head>
 
-
-
-<section class="features7 cid-raBwuiPryE" id="features7-l">
-
-
-
-
-
-    <div class="container">
-        <div class="media-container-row">
-            <div class="mbr-figure mb-4 mb-5" style="width: 55%;">
-                <img src="assets/images/braden-jarvis-517227-unsplash-720x1080.jpg" alt="Mobirise" title="">
-            </div>
-            <div class="counter-container media-content mb-4">
-                <h2 class="mb-4 mbr-fonts-style display-2">Our goal:</h2>
-                <h3 class="mbr-section-subtitle mbr-fonts-style mb-5 display-5">Combining a fun and promising community into a single worldwide network:</h3>
-                <div class="mb-5 half-list">
-                    <h4 class="list-counter">01.</h4>
-                    <h3 class="mb-3 content-list-title mbr-fonts-style display-5">Share photos</h3>
-                    <p class="mbr-fonts-style display-7">Share the best photos with friends and find new friends</p>
-                </div><!--
-                --><div class="mb-5 half-list">
-                    <h4 class="list-counter">02.</h4>
-                    <h3 class="mb-3 content-list-title mbr-fonts-style display-5">Chat with friends</h3>
-                    <p class="mbr-fonts-style display-7">Your friends are always here, wherever you are</p>
-                </div><!--
-                --><div class="mb-5 half-list">
-                    <h4 class="list-counter">03.</h4>
-                    <h3 class="mb-3 content-list-title mbr-fonts-style display-5">Using Filters</h3>
-                    <p class="mbr-fonts-style display-7"> Discover the best photo editor apps to edit and blur backgrounds, retouch your smartphone photographs, etc.</p>
-                </div><!--
-                --><div class="mb-5 half-list">
-                    <h4 class="list-counter">04.</h4>
-                    <h3 class="mb-3 content-list-title mbr-fonts-style display-5">Photo Ideas</h3>
-                    <p class="mbr-fonts-style display-7">We'll share own collection of photography ideas, and will help to find the way to inspire yourself.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="features1 cid-r7io8K1ARy" id="features1-i">
-
-
-    <div class="container-fluid">
-        <div class="row content-row">
-            <div class="mbr-figure col-lg-6">
-                <img src="assets/images/tiago-rosado-752257-unsplash-1904x1269.jpg" alt="" title="">
-            </div>
-            <div class="media-content px-5  mbr-white py-4 col-lg-6">
-
-                <div class="content-block">
-                    <h2 class="mbr-section-title mbr-fonts-style pb-4 display-2">About us</h2>
-
-                    <p class="mbr-text testimonial-text mbr-fonts-style display-7">
-                        We are a young, cheerful, promising team of lobsters who decided to make your quarantine everyday life a little more fun. We are students, but will soon be pros. Register and discover the amazing world with great opportunities. Gently, addiction may develop.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="mbr-section form3 cid-r7ioHdhVWM" id="form3-j">
-
-
-
-
-    <div class="container">
-        <div class="media-container-row">
-            <div class="col-md-10 col-lg-8 block-content">
-                <div data-form-type="formoid">
-                    <h2 class="mbr-fonts-style mb-4 display-2">Sign In</h2>
-                    <h3 class="mbr-section-subtitle mbr-fonts-style mb-4 mbr-light display-7">
-                        We are happy to invite you with us.</h3>
-                    <div data-form-alert="" hidden="">Thanks for filling out the form!</div>
-                    <form class="block mbr-form" action="#" method="post" data-form-title="Mobirise Form"><input type="hidden" name="email" data-form-email="true" value="2uh4z3wcwEFUOWKqlWVK7kHnKPnaGNW/mgkeGz0WrfCCfLrndplWqZUsxHrQ1TdwNGW/wxqL5tTGmgJCvKdvLWzPf4B7DpOnwJ16pdf8R8e2cb2e4L+sHhAZ+/KNfWmR">
-                        <div class="row">
-                            <div class="col-md-12" data-for="name">
-                                <input type="text" class="form-control input" name="name" data-form-field="Name" placeholder="Name" required="" id="name-form3-j">
-                            </div>
-                            <div class="col-md-12" data-for="email">
-                                <input type="text" class="form-control input" name="email" data-form-field="Email" placeholder="Email" required="" id="email-form3-j">
-                            </div>
-                            <div class="col-md-12" data-for="email">
-                                <input type="text" class="form-control input" name="pass" data-form-field="Pass" placeholder="Password" required="" id="email-form3-j">
-                            </div>
-                            <div class="input-group-btn col-md-12 mt-2"><button href="" type="submit" class="btn btn-form btn-bgr btn-warning display-4">Submit</button></div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="cid-r7ciD1CSwF" id="footer2-9">
-
-
-
-
-
-    <div class="container">
-
-
-        <div class="copyright align-center mb-3">
-            <p class="mb-0 text-copyright mbr-fonts-style mbr-white display-4">
-                © Copyright 2020 Venik Team - All Rights Reserved
-            </p>
-        </div>
-    </div>
-</section>
-
-
-<script src="{{asset('public/assets/web/assets/jquery/jquery.min.js')}}"></script>
-<script src="{{asset('public/assets/popper/popper.min.js')}}"></script>
-<script src="{{asset('public/assets/tether/tether.min.js')}}"></script>
-<script src="{{asset('public/assets/bootstrap/js/bootstrap.min.js')}}"></script>
-<script src="{{asset('public/assets/smoothscroll/smooth-scroll.js')}}"></script>
-<script src="{{asset('public/assets/dropdown/js/script.min.js')}}"></script>
-<script src="{{asset('public/assets/touchswipe/jquery.touch-swipe.min.js')}}"></script>
-<script src="{{asset('public/assets/parallax/jarallax.min.js')}}"></script>
-<script src="{{asset('public/assets/playervimeo/vimeo_player.js')}}"></script>
-<script src="{{asset('public/assets/theme/js/script.js')}}"></script>
-<script src="{{asset('public/assets/formoid/formoid.min.js')}}"></script>
-
-
+<body>
+@yield('content')
 </body>
-</html>
+
+<footer>
+
+
+</footer>
