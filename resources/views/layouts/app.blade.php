48b368a76622ee160d9327c6bd23be0ac70d7990
indigodness <alisher.relito@gmail.com>
2020-04-23 17:57:06 +0600

    Indigo commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,28 +19,36 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>SandWitch &mdash; Opportunity to project all your plans</title>
-    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Muli:300,400,700,900" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-{{--    <link rel="stylesheet" href="fonts/icomoon/style.css">--}}
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/style.css">
 
-{{--    <link rel="stylesheet" href="css/bootstrap.min.css">--}}
-{{--    <link rel="stylesheet" href="css/jquery-ui.css">--}}
-{{--    <link rel="stylesheet" href="css/owl.carousel.min.css">--}}
-{{--    <link rel="stylesheet" href="css/owl.theme.default.min.css">--}}
-{{--    <link rel="stylesheet" href="css/owl.theme.default.min.css">--}}
 
-{{--    <link rel="stylesheet" href="css/jquery.fancybox.min.css">--}}
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/main.js"></script>
 
-{{--    <link rel="stylesheet" href="css/bootstrap-datepicker.css">--}}
-
-{{--    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">--}}
-
-{{--    <link rel="stylesheet" href="css/aos.css">--}}
-
-{{--    <link rel="stylesheet" href="css/style.css">--}}
 </head>
 <body>
     <div id="app">
