0d00c7b183639d53cbde47638fa2d519e081e10a
indigodness <alisher.relito@gmail.com>
2020-04-27 21:32:50 +0600

    One more fix

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,8 +3,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
+<script src="{{ asset('js/app.js') }}" defer></script>
+
+
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Muli:300,400,700,900" rel="stylesheet">
 <link href="{{ asset('../css/app.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 <link rel="stylesheet" href="{{ asset('../fonts/icomoon/style.css')}}">
 <link rel="stylesheet" href="{{ asset('../css/bootstrap.min.css') }}">
